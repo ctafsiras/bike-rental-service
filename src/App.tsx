d283208c02e5f18cdef21aa4c0c2341455c68504
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2024-09-04 08:11:58 +0600

    Add new dependencies and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-function App() {
- 
+import { Button } from "@/components/ui/button";
 
+function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold ">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold ">Hello world!</h1>
+      <Button>Click me</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
